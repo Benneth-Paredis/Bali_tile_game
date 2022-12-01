daf536ae4d06ebb09eeeccd637130ed972ee6e96
Benneth Paredis <100223978+Benneth-Paredis@users.noreply.github.com>
2022-12-01 11:14:18 +0100

    All the current scripts of the game

--- Scripts/Rice_farm_tile.cs
+++ Scripts/Rice_farm_tile.cs
@@ -1,16 +1,16 @@
 using Godot;
 using System;
 
-public class Tile : Spatial
+public class Farm_tile : Tile
 {
-    public int xPos, yPos;
-
-    public Node n1, n2, n3, n4, n5, n6;
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
-    {   
-
+    {
+        
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
