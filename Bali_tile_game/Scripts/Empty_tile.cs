51e2c24eba48fc6987ce0d0db1a3e7459800090a
JonathanFrederickx <81532685+JonathanFrederickx@users.noreply.github.com>
2022-12-05 21:45:54 +0100

    Add files via upload

--- Bali_tile_game/Scripts/Empty_tile.cs
+++ Bali_tile_game/Scripts/Empty_tile.cs
@@ -3,10 +3,10 @@ using System;
 
 public class Empty_tile : Tile
 {
-    private Game_board game_board;
+	private Game_board game_board;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
 	{
 		game_board = (Game_board)GetParent();
 	}
@@ -27,7 +27,8 @@ public class Empty_tile : Tile
 			if(inputEventMouseButton.Pressed == true && inputEventMouseButton.ButtonIndex == 1)
 			{
 				GD.Print("Left Mouse button click");
-				spawnTile();
+				game_board.click_empty_tile(this.Translation.x, this.Translation.z);
+				QueueFree();
 			}
 			//Right mouse button click
 			if(inputEventMouseButton.Pressed == true && inputEventMouseButton.ButtonIndex == 2)
@@ -36,19 +37,6 @@ public class Empty_tile : Tile
 				this.Visible = false;
 			}
 		}
-	}
-
-	//Spawn 6 empty tiles surrounding the clicked tile and deleting it.
-	private void spawnTile()
-	{
-		game_board.spawnTile("empty_tile", this.Translation.x + 2*game_board.tileApothem, this.Translation.z);
-		game_board.spawnTile("empty_tile", this.Translation.x - 2*game_board.tileApothem, this.Translation.z);
-		game_board.spawnTile("empty_tile", this.Translation.x + game_board.tileApothem, this.Translation.z + 1.5f);
-		game_board.spawnTile("empty_tile", this.Translation.x + game_board.tileApothem, this.Translation.z - 1.5f);
-		game_board.spawnTile("empty_tile", this.Translation.x - game_board.tileApothem, this.Translation.z + 1.5f);
-		game_board.spawnTile("empty_tile", this.Translation.x - game_board.tileApothem, this.Translation.z - 1.5f);
-
-		QueueFree();
 	}
 }
 
